26187f1105bafc293712b68b2413d7c1e6e33579
Rayne Peng <lei@Rayne-MacBook-Pro.local>
2021-01-22 23:52:11 +0800

    init commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Object Oriented JavaScript
+1. class constructor
+2. property and function 
+# localStorage
+1. setItem()
+2. getItem()
+3. removeItem()
+4. clear()
+# firebase database
+1. `<script src="https://www.gstatic.com/firebasejs/8.2.4/firebase-app.js"></script>`
+2. `<script src="https://www.gstatic.com/firebasejs/8.2.4/firebase-firestore.js"></script>`
+3. firebase config
+4. chat.js with the main class function, ui.js with the dom manipulation, app.js where it's initiated.
---
+++ scripts/app.js
@@ -0,0 +1,54 @@
+// Dom queries
+const chatList = document.querySelector(".chat-list");
+const newChatForm = document.querySelector(".new-chat");
+const newNameForm = document.querySelector(".new-name");
+const updateMessg = document.querySelector(".update-mssg");
+const rooms = document.querySelector('.chat-rooms');
+
+// add a new chat
+newChatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = newChatForm.message.value.trim();
+  chatroom
+    .addChat(message)
+    .then(() => {
+      newChatForm.reset();
+    })
+    .catch((err) => console.log(err));
+});
+
+// update username
+newNameForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //   update name via chatroom
+  const newName = newNameForm.name.value.trim();
+  chatroom.updateName(newName);
+  //   rest the form
+  newNameForm.reset();
+//   show then hide the update message
+    updateMessg.innerHTML = `Your name was updated to ${newName}`;
+    setTimeout(()=> {updateMessg.innerHTML = ''}, 3000);
+});
+
+// update the chat room
+rooms.addEventListener('click', e => {
+    if(e.target.tagName === 'BUTTON'){
+        chatUI.clear();
+        chatroom.updateRoom(e.target.getAttribute('id'));
+        chatroom.getChats(chatchat => chatUI.render(chatchat));
+
+    }
+})
+
+// check local storage for a name
+const username = localStorage.username ? localStorage.username : 'anon';
+
+// class instances
+const chatUI = new ChatUI(chatList);
+const chatroom = new Chatroom("general", username);
+
+// get chats and render
+chatroom.getChats((data) => {
+  // console.log(data);
+  chatUI.render(data);
+});
